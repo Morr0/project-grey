b7bef1572f96cbb10cc35d0ca4dd32442962fbc0
Rami <ramihikmat10@gmail.com>
2019-12-23 09:39:48 +1100

    test commit

--- core/src/rami/project/grey/ui/screens/PlayScreen.java
+++ core/src/rami/project/grey/ui/screens/PlayScreen.java
@@ -173,5 +173,5 @@ public class PlayScreen extends BaseScreen {
         Chika.ChikaSize size = entity.getSize();
         Vector2 dimensions = new Vector2(gCal.gridWidth / size.towWeight, gCal.gridHeight / size.towWeight);
         batch.draw(bigChika.getTexture(), pos.x, pos.y, dimensions.x, dimensions.y);
-    }
+    } 
 }
